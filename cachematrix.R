870d2753408e7501e17e9215cb7e8b8a2497bc0d
Hai H Nguyen <nguyenhohai88@gmail.com>
2015-09-26 13:44:24 -0400

    upload assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,67 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(data = matrix()) {
+  cacheMatrix <- NULL
+  setData <-function(y)
+  {
+    data <<-y
+    cacheMatrix<<-NULL
+  }
+  getData <-function(){
+    data
+  }
+  setMatrix<-function(m){
+    cacheMatrix <<-m
+  }
+  getMatrix <-function(){
+    cacheMatrix
+  }
+  list(setData=setData,getData=getData,setMatrix=setMatrix,getMatrix=getMatrix)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(functionsList, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  cacheMatrix<-functionsList$getMatrix()
+  if(!is.null(cacheMatrix)){
+    message("getting cached matrix")
+    return (cacheMatrix)
+  }
+  data<-functionsList$getData();
+  inverMatrix<- solve(data)
+  functionsList$setMatrix(inverMatrix)
+  inverMatrix
 }
+
+## Testing function on R console
+## Step1: create invertible Square Matrix 
+## matrix2 <- matrix(c(4,2,7,6),2)
+## matrix2
+##       [,1] [,2]
+## [1,]    4    7
+## [2,]    2    6
+##
+## Step2: intialize functions list with matrix2 as input data
+## functionsList <- makeCacheMatrix(matrix2)  
+##
+## Step3: solve the maxtrix2 to get invert matrix of it
+## invertMatrix2<-cacheSolve(functionsList)
+## invertMatrix2
+##       [,1] [,2]
+## [1,]  0.6 -0.7
+#3 [2,] -0.2  0.4
+##
+## Step4: run the solve again to get invertmatrix from cache
+## invertMatrix2<-cacheSolve(functionsList)
+## getting cached matrix
+##
+## Step 5: double check the invert matrix by multiplying both matrix
+## matrix2 %*% invertMatrix2
+##      [,1] [,2]
+## [1,]    1    0
+## [2,]    0    1
+##
+##
